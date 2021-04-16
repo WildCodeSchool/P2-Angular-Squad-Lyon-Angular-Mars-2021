04a1e2533d6c7480afdc11d62315f7defe7795bc
fethibettahar9@gmail.com <fethibettahar9@gmail.com>
2021-04-16 12:44:36 +0200

    commit du vendredi 16 avril 12h44 slider fonctionnel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SliderJoueusesComponent } from './slider-joueuses/slider-joueuses.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderJoueusesComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+  
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:  [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/slider-joueuses/slider-joueuses.component.ts
@@ -0,0 +1,16 @@
+import { Component ,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-joueuses',
+  templateUrl: './slider-joueuses.component.html',
+  styleUrls: ['./slider-joueuses.component.css']
+})
+export class SliderJoueusesComponent implements OnInit {
+  
+
+  constructor() { }
+
+  ngOnInit(){}
+
+  
+}

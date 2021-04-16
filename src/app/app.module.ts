ccba7d98deaae202704cf336bda3c0910f9ebce6
hermann <adinsihermann1@gmail.com>
2021-04-16 10:08:26 +0200

    push de la page Qui sommes-nous

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuisommesnousComponent } from './quisommesnous/quisommesnous.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuisommesnousComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/quisommesnous/quisommesnous.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quisommesnous',
+  templateUrl: './quisommesnous.component.html',
+  styleUrls: ['./quisommesnous.component.css']
+})
+export class QuisommesnousComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

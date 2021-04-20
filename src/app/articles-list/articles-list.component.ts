93b483b984092dcba59dd3d1785b9a4d5869a354
RangoDisco <diasmaxime1@gmail.com>
2021-04-20 14:44:33 +0200

    added articlesList

---
+++ src/app/articles-list/articles-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-articles-list',
+  templateUrl: './articles-list.component.html',
+  styleUrls: ['./articles-list.component.css'],
+})
+export class ArticlesListComponent implements OnInit {
+  Articles: Object[] = [
+    {
+      link: '#',
+      img: '../../assets/img/article1.jpg',
+      sport: 'FOOT',
+      date: '15/04/2021',
+      desc: 'Doublé pour Nikita Parris ce week-end',
+    },
+    {
+      link: '#',
+      img: '../../assets/img/article2.jpg',
+      sport: 'FOOT',
+      date: '15/04/2021',
+      desc: "Victoire et qualification en Finale pour l'OL",
+    },
+    {
+      link: '#',
+      img: '../../assets/img/article3.jpg',
+      sport: 'FOOT',
+      date: '15/04/2021',
+      desc: "L'OL perd de son avance après sa défaite face au PSG",
+    },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}

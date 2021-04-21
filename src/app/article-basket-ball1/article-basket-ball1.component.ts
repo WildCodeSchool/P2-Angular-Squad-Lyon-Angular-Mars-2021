ab1c0c4ec5d433012425da0131cc336a9795a330
fethibettahar9@gmail.com <fethibettahar9@gmail.com>
2021-04-21 21:15:59 +0200

    mise à jour DES articles

--- src/app/article-basket-ball1/article-basket-ball1.component.ts
+++ src/app/article-basket-ball1/article-basket-ball1.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-basket-ball1.component.css']
 })
 export class ArticleBasketBall1Component implements OnInit {
-
+  image1:string="assets/img/imgArticleS/Des-filles-dans-la-melee.jpg";
+  image2:string="assets/img/imgArticleS/istockphoto-874423892-612x612.jpg";
   constructor() { }
 
   ngOnInit(): void {
--- src/app/article-tennis1/article-tennis1.component.ts
+++ src/app/article-tennis1/article-tennis1.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleTennis1Component implements OnInit {
 
+  image1:string="assets/img/imgArticleS/Des-filles-dans-la-melee.jpg";
+  image2:string="assets/img/imgArticleS/istockphoto-874423892-612x612.jpg";
   constructor() { }
 
   ngOnInit(): void {

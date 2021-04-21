6afc13ead554f4f8703794a68a11407e74c49068
fethibettahar9@gmail.com <fethibettahar9@gmail.com>
2021-04-21 10:06:56 +0200

    final + fichier asset/image/jpg

--- src/app/slider-joueuses/slider-joueuses.component.ts
+++ src/app/slider-joueuses/slider-joueuses.component.ts
@@ -1,13 +1,14 @@
 import { Component ,OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-slider-joueuses',
   templateUrl: './slider-joueuses.component.html',
   styleUrls: ['./slider-joueuses.component.css']
 })
-export class SliderJoueusesComponent implements OnInit {
+export class SliderJoueusesComponent implements OnInit{
   
-
+  
   constructor() { }
 
   ngOnInit(){}

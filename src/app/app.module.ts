359fc87823c2160cd4c47f4245925e1077e79a32
hermann <adinsihermann1@gmail.com>
2021-04-22 19:46:08 +0200

    le component direct en cours

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { QuisommesnousComponent } from './quisommesnous/quisommesnous.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DirectencoursComponent } from './directencours/directencours.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DemoMaterialModule } from './material.module';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuisommesnousComponent
+    QuisommesnousComponent,
+    DirectencoursComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    MatTabsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/directencours/directencours.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Pays {
+  value: string;
+  viewValue: string;
+}
+
+interface Direct {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-directencours',
+  templateUrl: './directencours.component.html',
+  styleUrls: ['./directencours.component.css']
+})
+export class DirectencoursComponent implements OnInit {
+
+  lesPays: Pays[] = [
+    {value: 'Angleterre-0', viewValue: 'Angleterre'},
+    {value: 'Soudan-1', viewValue: 'Soudan'},
+    {value: 'Maroc-2', viewValue: 'Maroc'}
+  ];
+
+ /*mesdirects: Direct[] = [
+    {value: 'Angleterre-0', viewValue: 'Angleterre'},
+    {value: 'Soudan-1', viewValue: 'Soudan'},
+    {value: 'Maroc-2', viewValue: 'Maroc'}
+  ];*/
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
